03d63e2e357418c5ff954bcda6257580e53fcf85
tingwong <tingwong0704@gmail.com>
2025-06-03 15:54:43 +0100

    Initial commit

---
+++ src/main/java/org/example/testaction/TestActionApplication.java
@@ -0,0 +1,13 @@
+package org.example.testaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestActionApplication.class, args);
+    }
+
+}
